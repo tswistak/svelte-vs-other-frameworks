90c1afb6dbbbfe4ecfceb8e46583ca11cdfb2603
Tomasz Świstak <swistak.tomasz@outlook.com>
2020-11-10 22:18:48 +0100

    added svelte apps

---
+++ Svelte/click-counter/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	let clicks = 0;
+
+	function increment() {
+	  clicks++;
+	}
+
+	function decrement() {
+	  clicks--;
+	}
+</script>
+
+<h1>Clicked {clicks} times!</h1>
+<button on:click={increment}>Add click</button>
+<button on:click={decrement}>Remove click</button>
